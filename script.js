c66c6d35474182057c8e5903ac9c4a08114ff983
Shah Alam <shahalam141072@gmail.com>
2023-07-20 11:50:25 +0600

    done

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const canvas = document.querySelector('canvas'),
-ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d");
 brushWidth = 5;
 
+let toolBtns = document.querySelectorAll('.tool');
 let isDrawing = false;
-window.addEventListener('load',function(){
+
+
+
+
+
+
+
+
+// all funcitons:
+window.addEventListener('load', function () {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 });
+
 const startDrawing = () => {
     isDrawing = true;
     ctx.beginPath();
@@ -14,11 +25,21 @@ const startDrawing = () => {
 }
 
 const drawing = function (e) {
-    if(!isDrawing) return;
+    if (!isDrawing) return;
     ctx.lineTo(e.offsetX, e.offsetY); // drawing line to canvas;
     ctx.stroke();
 }
 
-canvas.addEventListener('mousedown',startDrawing);
-canvas.addEventListener('mousemove',drawing);
-canvas.addEventListener('mouseup',() => isDrawing = false)
+toolBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        // if (btn.contains('.active')) {
+        //     btn.classList.remove('active')
+        // }
+        btn.classList.add('active');
+
+    })
+});
+
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', drawing);
+canvas.addEventListener('mouseup', () => isDrawing = false)
