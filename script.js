ce72975e74e7ffd1491dddfc641e1507490f5778
Shah Alam <shahalam141072@gmail.com>
2023-07-20 15:55:07 +0600

    done

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const canvas = document.querySelector('canvas'),
     ctx = canvas.getContext("2d");
 let brushWidth = 5;
+let selectedColor = '#000';
 
 let toolBtns = document.querySelectorAll('.tool');
 let fillColor = document.querySelector('#checkbox');
-let color = document.querySelector('#color').value;
-console.log(color)
+let sizeBrush = document.querySelector('#range');
+let color = document.querySelector('#color');
+let canvas_clear = document.querySelector('.canvas_clear');
+let download_image = document.querySelector('.download_image');
 let prevMouseX, prevMouseY, snapshot,
     isDrawing = false,
     selectedTool = 'brush';
@@ -34,28 +37,23 @@ const drawRect = (e) => {
 }
 
 const drawCircle = (e) => {
-     
+
     ctx.beginPath();
-    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX),2) + Math.pow((prevMouseY- e.offsetY),2));
-    ctx.arc(prevMouseX,prevMouseY,radius,0,2*Math.PI);
+    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
     fillColor.checked ? ctx.fill() : ctx.stroke();
-    ctx.fillStyle = "red";
-     
+
+
 }
 
 const drawTriangle = (e) => {
     ctx.beginPath();
-    ctx.moveTo(prevMouseX,prevMouseY);
-    ctx.lineTo(e.offsetX,e.offsetY)
-    ctx.lineTo(prevMouseX*2 -e.offsetX,e.offsetY)
+    ctx.moveTo(prevMouseX, prevMouseY);
+    ctx.lineTo(e.offsetX, e.offsetY)
+    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY)
     ctx.closePath();
-    fillColor.checked ? ctx.fill()  : ctx.stroke()
+    fillColor.checked ? ctx.fill() : ctx.stroke()
 }
-
-
-
-
-
 
 const startDrawing = (e) => {
     prevMouseX = e.offsetX;
@@ -63,6 +61,8 @@ const startDrawing = (e) => {
     isDrawing = true;
     ctx.beginPath();
     ctx.lineWidth = brushWidth;
+    ctx.strokeStyle = selectedColor;
+    ctx.fillStyle = selectedColor;
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
@@ -82,7 +82,7 @@ const drawing = function (e) {
         drawCircle(e);
     }
 
-    else if(selectedTool === 'triangle'){
+    else if (selectedTool === 'triangle') {
         drawTriangle(e);
     }
 
@@ -97,6 +97,26 @@ toolBtns.forEach(btn => {
         console.log(selectedTool)
     })
 });
+
+sizeBrush.addEventListener('change', function () {
+    brushWidth = sizeBrush.value
+    console.log(brushWidth)
+});
+
+color.addEventListener('input', function () {
+    selectedColor = this.value;
+});
+
+canvas_clear.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+download_image.addEventListener('click', () => {
+    const link = document.createElement("a");
+    link.download = `${Date.now()}.jpg`;
+    link.href = canvas.toDataURL();
+    link.click();
+})
 
 canvas.addEventListener('mousedown', startDrawing);
 canvas.addEventListener('mousemove', drawing);
